8a4ddf7f79851bc53b4963aecfcb3d3b52028232
flydes <ershovmihail1881@gmail.com>
2023-08-27 21:45:08 +0300

    refactoring mapping quill v3

--- src/main/scala/com/ecom/point/utils/types/package.scala
+++ src/main/scala/com/ecom/point/utils/types/package.scala
@@ -10,18 +10,16 @@ import java.util.UUID
 package object types {
 	implicit val ordInstant: Ord[Instant] = Ord.fromScala[Instant]
 	implicit val eqUUID: Equal[UUID] = Equal.fromScala[UUID]
+	implicit val ordUUID: Ord[UUID] = Ord.fromScala[UUID]
 	
 	abstract class StringType extends RichNewtype[String]
-	
 	abstract class UUIDType extends RichNewtype[UUID]
+	abstract class InstantType extends RichNewtype[Instant]
 	
-	abstract class InstantType extends RichNewtype[Instant] {self =>
-		implicit val ord: Ord[self.Type] = Ord[Instant].contramap {unwrap}
-	}
-	
-	abstract class RichNewtype[A : JsonCodec : Schema : Equal] extends Newtype[A] { self =>
+	abstract class RichNewtype[A : JsonCodec : Schema : Equal : Ord] extends Newtype[A] { self =>
 		implicit val equiv: A <=> Type = Equivalence(wrap, unwrap)
 		implicit val equal: Equal[Type] = implicitly[Equal[A]].contramap(unwrap)
+		implicit val ord: Equal[Type] = implicitly[Ord[A]].contramap(unwrap)
 		implicit val jsonCodec: JsonCodec[Type] = implicitly[JsonCodec[A]].transform(wrap, unwrap)
 		implicit val schemeConverter: Schema[Type] = implicitly[Schema[A]].transform(wrap, unwrap)
 		
